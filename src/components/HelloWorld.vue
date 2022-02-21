ea746052275c72e316d1007db28b14849b256873
nina hedman <ni.hedman@gmail.com>
2022-02-21 19:02:28 +0100

    Add styling users

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,8 +2,17 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
 
-    <div v-for="(user, i) in users" :key="i + 'user'">
-      <p>{{ user.id }}: {{ user.name }}</p>
+    <div class="users">
+      <div
+        class="user"
+        :class="{ inactive: user.status === 'inactive' }"
+        v-for="(user, i) in users"
+        :key="i + 'user'"
+      >
+        <h3>{{ user.name }}</h3>
+        <p>{{ user.email }}</p>
+        <p>{{ user.gender }}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -34,10 +43,10 @@ export default {
     //   console.log(data);
     // });
     this.getAllUsers();
-    this.updateUser("4897", {
-      name: "Mimmi",
-    });
-    this.deleteUser("3884");
+    // this.updateUser("4897", {
+    //   name: "Mimmi",
+    // });
+    // this.deleteUser("3884");
     // this.getUser("3894");
   },
 
@@ -103,7 +112,8 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
-  margin: 40px 0 0;
+  /* border-bottom: solid; */
+  font-size: 1.3rem;
 }
 ul {
   list-style-type: none;
@@ -115,5 +125,72 @@ li {
 }
 a {
   color: #42b983;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+.hello {
+  display: flex;
+  justify-content: space-around;
+  flex-direction: column;
+  align-items: center;
+}
+
+.users {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  width: 80vw;
+}
+
+.user {
+  display: flex;
+  justify-content: space-around;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+  margin: 5vw;
+  padding: 1rem;
+
+  font-size: 0.9rem;
+  word-break: break-word;
+  background: #c2efdc;
+  border-radius: 1rem;
+  transition: transform 0.2s;
+  -webkit-box-shadow: 5px 4px 7px 2px rgba(0, 0, 0, 0.4),
+    5px 4px 7px 2px rgba(0, 0, 0, 0.4);
+  box-shadow: 5px 4px 7px 2px rgba(0, 0, 0, 0.4),
+    5px 4px 7px 2px rgba(0, 0, 0, 0.4);
+}
+
+.user:hover {
+  cursor: pointer;
+  transform: scale(1.05);
+  background: #81b8a1;
+}
+
+.inactive {
+  opacity: 50%;
+}
+
+/* Small devices (portrait tablets and large phones, 600px and up) */
+@media only screen and (min-width: 600px) {
+  .user {
+    width: 45%;
+    margin: 2%;
+  }
+}
+
+/* Large devices (laptops/desktops, 992px and up) */
+@media only screen and (min-width: 992px) {
+  .users {
+    width: 90vw;
+  }
+  .user {
+    width: 20%;
+  }
 }
 </style>
